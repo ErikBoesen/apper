e2dacf0d24be35ea627f9a2d3d58e510dd57378e
ErikBoesen <me@erikboesen.com>
2018-06-02 10:34:57 -0400

    Start of Info.plist creation

--- appify.sh
+++ appify.sh
@@ -3,6 +3,7 @@
 script_path=$1
 app_path=$2
 app_name="$(basename $app_path)"
+app_shortname="${app_name%.app}"
 
 shift
 shift
@@ -23,12 +24,29 @@ esac
 done
 
 contents_path="$app_path/Contents"
+info_path="$contents_path/Info.plist"
 macos_path="$contents_path/MacOS"
 resources_path="$contents_path/Resources"
 
+mkdir -p "$contents_path"
+cat > "$info_path" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+	<key>CFBundleExecutable</key>
+	<string></string>
+	<key>CFBundleIconFile</key>
+	<string></string>
+</dict>
+</plist>
+EOF
+defaults write "$info_path" CFBundleExecutable -string "$app_shortname"
+
 mkdir -p "$macos_path"
-mv "$script_path" "$macos_path/${app_name%.app}"
+mv "$script_path" "$macos_path/$app_shortname"
 if [[ ! -z "$icon_path" ]]; then
     mkdir -p "$resources_path"
-    cp "$icon_path" "$resources_path/${app_name%.app}.icns"
+    defaults write "$info_path" CFBundleIconFile -string "$app_shortname.icns"
+    cp "$icon_path" "$resources_path/$app_shortname.icns"
 fi
